81c4ef953bfd1a4cdc81c478c41c7f5bc1cba01e
rodriguezh21 <rodriguezh21@gmail.com>
2020-04-11 15:09:55 -0500

    email and avatar work

--- content/index.js
+++ content/index.js
@@ -1,6 +1,7 @@
-//const api = require("./utils/api");
+const api = require("../utils/api");
 const fs = require("fs")
 const inquirer = require("inquirer");
+const axios = require('axios');
 const generateMarkdown = require("../utils/generateMarkdown");
 
 
@@ -58,7 +59,12 @@ const questions = [
   message: "Any questions or comments about the future development of your project?",
   name: "questions"
   
-}
+},
+{
+  type: "input",
+  message: "What is your github user name?",
+  name: "username"
+} 
 ];
 
 
@@ -70,15 +76,15 @@ console.log("Successfully wrote to questions.json file");
 });*/
 
 
-function init() {
+async function init() {
   // console.log("API Data: "+JSON.stringify(api));
-  inquirer.prompt(questions).then(function(res){
-    console.log("Res: "+JSON.stringify(res));
-    generateMarkdown(res);
-    fs.writeFile("test.txt",generateMarkdown(res),function(err,data){
-      console.log(data);
+  let response = await inquirer.prompt(questions)
+  let apiRes = await api(response.username)
+    console.log(apiRes.data)
+    //generateMarkdown(res);
+    fs.writeFile("test.txt",generateMarkdown(response, apiRes.data),function(err,data){
+      // console.log(data);
     })
-  })
 }
 
 init();
